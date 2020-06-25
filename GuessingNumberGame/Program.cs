61787e4264c3bba21e66a186409e31ce10c8ad64
juanchoguillo <juancardona0607@gmail.com>
2020-06-25 15:58:32 -0400

    with the one ++ and -- when startNumber and LimitNumber get so close

--- GuessingNumberGame/Program.cs
+++ GuessingNumberGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace GuessingNumberGame
 {
@@ -15,17 +17,22 @@ namespace GuessingNumberGame
             Console.WriteLine("Now to make this game more intreresting give me the limit number of the guessing");
             Console.WriteLine("for example you are gonna think  in a number beteween 1 and <The limit number>");
 
-            Console.WriteLine(" So... Give me please the limit number");
+            Console.WriteLine(" So... Give me please the limit number, it can be as high as you want");
 
             var limitNumber = int.Parse(Console.ReadLine());
+            var normalSteps = limitNumber;
             var startPoint = 0;
             var foundNumber = true;
-            
-                Console.WriteLine($"Ok, so you want me to Guess a number between 1 and {limitNumber }");
+            var myList = new List<int>();
+            var repeatVariable = 1;
+
+
+            Console.WriteLine($"Ok, so you want me to Guess a number between 1 and {limitNumber }");
 
             do
             {
-                Console.WriteLine($"is your number :  {((limitNumber - startPoint)/2) + startPoint}");
+                Console.WriteLine($"is your number :  {((limitNumber - startPoint)/2) + startPoint} ?");
+
 
                 var userResponse = Console.ReadLine().ToLower();
 
@@ -36,19 +43,67 @@ namespace GuessingNumberGame
 
                 }else if(userResponse == "l")
                 {
-                    Console.WriteLine("Ok lets try again");
-                    limitNumber = (limitNumber/2) + (startPoint / 2);
+                  // myList.Add()
+                    repeatVariable++;
+
+                    if( limitNumber - startPoint > 1)
+                    {
+                        Console.WriteLine("Ok lets try again");
+                        limitNumber = (limitNumber / 2) + (startPoint / 2);
+                    }
+                    else
+                    {
+                         Console.WriteLine($"is your number :  {limitNumber} ?");
+                         userResponse = Console.ReadLine().ToLower();
+                        while (userResponse != "y" )
+                        {
+                            limitNumber--;
+                            repeatVariable++;
+
+                        }
+                        Console.WriteLine("I told you I was going to guess");
+                        foundNumber = false;
+
+
+                    }
+
                 }
+
                 else if (userResponse == "h")
                 {
-                    Console.WriteLine("Ok lets try again");
-                    startPoint = ((limitNumber - startPoint) / 2) + startPoint;
+                    repeatVariable++;
+
+                    if (limitNumber - startPoint > 1)
+                    {
+                        Console.WriteLine("Ok lets try again");
+                        startPoint = ((limitNumber - startPoint) / 2) + startPoint;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"is your number :  {limitNumber} ?");
+                        userResponse = Console.ReadLine().ToLower();
+                        while (userResponse != "y")
+                        {
+                            limitNumber++;
+                            repeatVariable++;
+
+                        }
+
+                        Console.WriteLine("I told you I was going to guess");
+                        foundNumber = false;
+
+
+                    }
+
                 }
                 else
                 {
                     Console.WriteLine("Sorry i dont get it");
                 }
             } while (foundNumber );
+
+            Console.WriteLine($"For a normal code it would take {normalSteps} steps, but for me it just took {repeatVariable}");
+            Console.WriteLine("and that is cuz I am a TrueCoder ");
 
             
 
